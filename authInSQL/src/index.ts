a3d7ab5981cae39e7e2c84ddd4bf70fd8048a3d5
Nitesh <niteshsengar9831@gmail.com>
2026-03-31 16:57:58 +0530

    create http server

--- authInSQL/src/index.ts
+++ authInSQL/src/index.ts
@@ -1,1 +1,16 @@
-console.log("hellodd");
+import { createServer } from "node:http";
+
+async function main() {
+  try {
+    const server = createServer();
+    const PORT: number = 8080;
+    server.listen(PORT, () => {
+      console.log(`http server is running on port:${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+main();
